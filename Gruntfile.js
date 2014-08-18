88e3a4737abb9a83b6b3fa7b91a4ce727e9ae9bc
tecfu <help@tecfu.com>
2014-08-18 10:40:55 -0400

    playing with travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,7 +56,7 @@ module.exports = function(grunt) {
             ,travis: {
                 configFile: 'karma.conf.js',
                 singleRun: true,
-                browsers: ['Chrome']
+                browsers: ['PhantomJS']
             }
         },
         
