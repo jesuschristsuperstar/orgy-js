a347a07215ee8cfe175c55820e8950502c0281ef
tecfu <help@tecfu.com>
2014-09-02 11:05:26 -0400

    Updated Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,12 +16,26 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON('package.json')
         
-        uglify: {
+        ,options : {
+            timestamp : (function(){
+
+                //A FORMATTED TIMESTAMP STRING FOR BACKUP NAMING
+                var d = new Date(),dstr = '';
+                dstr = ('0' + d.getHours()).slice(-2)
+                + ':' + ('0' + d.getMinutes()).slice(-2)
+                + ':' + ('0' + d.getSeconds()).slice(-2);
+
+                return dstr;
+
+            }())
+        }
+        
+        ,uglify: {
             all: {
                 options: {
-                    banner: '/** \n<%= pkg.name %>: A queue and deferred library that is so very hot right now. \nVersion: <%= pkg.version %> \nBuilt: <%= grunt.template.today("yyyy-mm-dd") %> \nAuthor: <%= pkg.author %>  \n*/\n'
+                    banner: '/** \n<%= pkg.name %>: A queue and deferred library that is so very hot right now. \nVersion: <%= pkg.version %> \nBuilt: <%= grunt.template.today("yyyy-mm-dd") %> <%= options.timestamp %>\nAuthor: <%= pkg.author %>  \n*/\n'
                     ,mangle : true
                     ,compress : true
                     ,drop_debugger : false
@@ -33,7 +47,7 @@ module.exports = function(grunt) {
             }
             ,devel: {
                 options: {
-                    banner: '/** \n<%= pkg.name %>: A queue and deferred library that is so very hot right now. \nVersion: <%= pkg.version %> \nBuilt: <%= grunt.template.today("yyyy-mm-dd") %> \nAuthor: <%= pkg.author %>  \n*/\n'
+                    banner: '/** \n<%= pkg.name %>: A queue and deferred library that is so very hot right now. \nVersion: <%= pkg.version %> \nBuilt: <%= grunt.template.today("yyyy-mm-dd") %> <%= options.timestamp %>\nAuthor: <%= pkg.author %>  \n*/\n'
                     ,sourceMap : true
                     ,sourceMapIncludeSources : true
                     /*MANGLED VARIABLES WILL NOT MAP CORRECTLY TO SOURCE MAP*/
