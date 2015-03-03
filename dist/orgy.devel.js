de8e685174f89e25a38c45b70b2b977cace07b68
5fec8fdadc460cb8540a901f08ef288b <james@tecfu.com>
2015-03-03 15:28:50 -0500

    updated package.json / build

--- dist/orgy.devel.js
+++ dist/orgy.devel.js
@@ -1,7 +1,7 @@
 /** 
 orgy: Globally accessible queues [of deferreds] that wait for an array of dependencies [i.e. files,rpcs,timers,events] and an optional resolver function before settling. Returns a thenable. 
-Version: 1.7.5 
-Built: 2014-12-30 21:23:05
+Version: 1.7.6 
+Built: 2015-03-03 15:28:33
 Author: tecfu.com <help@tecfu.com> (http://github.com/tecfu)  
 */
 
