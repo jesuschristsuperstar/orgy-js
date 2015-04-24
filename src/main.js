fc0b07c3700f532c1b2ff1a5cf74e27c388ee9fd
tecfu <help@tecfu.com>
2015-04-24 09:09:19 -0700

    Added test to make sure _btrc works on .define

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var Deferred = require('./deferred.js'),
-    Queue = require('./queue.js'),
-    Cast = require('./cast.js'),
-    Config = require('./config.js');
+		Queue = require('./queue.js'),
+		Cast = require('./cast.js'),
+		Config = require('./config.js');
 
 module.exports = {
 
@@ -18,52 +18,52 @@ module.exports = {
 *
 * @param {string} id A unique id you give to the object
 * @param {mixed}  data The value that the object is assigned
-* @param {object} options 
+* @param {object} options
 - <b>dependencies</b> {array}
 - <b>resolver</b> {function(<i>assignedValue</i>,<i>deferred</i>}
 * @returns {object} resolved deferred
 */
 define : function(id,data,options){
 
-    var def;
-    options = options || {};
-    options.dependencies = options.dependencies || null;
-    options.resolver = options.resolver || null;
+		var def;
+		options = options || {};
+		options.dependencies = options.dependencies || null;
+		options.resolver = options.resolver || null;
 
-    //test for a valid id
-    if(typeof id !== 'string'){
-      Config.debug("Must set id when defining an instance.");
-    }
+		//test for a valid id
+		if(typeof id !== 'string'){
+			Config.debug("Must set id when defining an instance.");
+		}
 
-    //Check no existing instance defined with same id
-    if(Config.list[id] && Config.list[id].settled === 1){
-      return Config.debug("Can't define " + id + ". Already resolved.");
-    }
+		//Check no existing instance defined with same id
+		if(Config.list[id] && Config.list[id].settled === 1){
+			return Config.debug("Can't define " + id + ". Already resolved.");
+		}
 
-    options.id = id;
+		options.id = id;
 
-    if(options.dependencies !== null
-      && options.dependencies instanceof Array){
-      //Define as a queue - can't autoresolve because we have deps
-      var deps = options.dependencies;
-      delete options.dependencies;
-      def = Queue(deps,options);
-    }
-    else{
-      //Define as a deferred
-      def = Deferred(options);
+		if(options.dependencies !== null
+			&& options.dependencies instanceof Array){
+			//Define as a queue - can't autoresolve because we have deps
+			var deps = options.dependencies;
+			delete options.dependencies;
+			def = Queue(deps,options);
+		}
+		else{
+			//Define as a deferred
+			def = Deferred(options);
 
-      //Try to immediately settle [define]
-      if(options.resolver === null
-        && (typeof options.autoresolve !== 'boolean'
-        || options.autoresolve === true)){
-        //prevent future autoresove attempts [i.e. from xhr response]
-        def.autoresolve = false;
-        def.resolve(data);
-      }
-    }
+			//Try to immediately settle [define]
+			if(options.resolver === null
+				&& (typeof options.autoresolve !== 'boolean'
+				|| options.autoresolve === true)){
+				//prevent future autoresove attempts [i.e. from xhr response]
+				def.autoresolve = false;
+				def.resolve(data);
+			}
+		}
 
-    return def;
+		return def;
 },
 
 
@@ -78,12 +78,12 @@ define : function(id,data,options){
  * @returns {object} deferred | queue | null
  */
 get : function(id){
-  if(Config.list[id]){
-    return Config.list[id];
-  }
-  else{
-    return null;
-  }
+	if(Config.list[id]){
+		return Config.list[id];
+	}
+	else{
+		return null;
+	}
 },
 
 
@@ -103,45 +103,45 @@ get : function(id){
  */
 assign : function(tgt,arr,add){
 
-    add = (typeof add === "boolean") ? add : 1;
+		add = (typeof add === "boolean") ? add : 1;
 
-    var id,q;
-    switch(true){
-        case(typeof tgt === 'object' && typeof tgt.then === 'function'):
-            id = tgt.id;
-            break;
-        case(typeof tgt === 'string'):
-            id = tgt;
-            break;
-        default:
-            return Config.debug("Assign target must be a queue object or the id of a queue.",this);
-    }
+		var id,q;
+		switch(true){
+				case(typeof tgt === 'object' && typeof tgt.then === 'function'):
+						id = tgt.id;
+						break;
+				case(typeof tgt === 'string'):
+						id = tgt;
+						break;
+				default:
+						return Config.debug("Assign target must be a queue object or the id of a queue.",this);
+		}
 
-    //IF TARGET ALREADY LISTED
-    if(Config.list[id] && Config.list[id].model === 'queue'){
-        q = Config.list[id];
+		//IF TARGET ALREADY LISTED
+		if(Config.list[id] && Config.list[id].model === 'queue'){
+				q = Config.list[id];
 
-        //=> ADD TO QUEUE'S UPSTREAM
-        if(add){
-            q.add(arr);
-        }
-        //=> REMOVE FROM QUEUE'S UPSTREAM
-        else{
-            q.remove(arr);
-        }
-    }
-    //CREATE NEW QUEUE AND ADD DEPENDENCIES
-    else if(add){
-        q = Queue(arr,{
-            id : id
-        });
-    }
-    //ERROR: CAN'T REMOVE FROM A QUEUE THAT DOES NOT EXIST
-    else{
-        return Config.debug("Cannot remove dependencies from a queue that does not exist.",this);
-    }
+				//=> ADD TO QUEUE'S UPSTREAM
+				if(add){
+						q.add(arr);
+				}
+				//=> REMOVE FROM QUEUE'S UPSTREAM
+				else{
+						q.remove(arr);
+				}
+		}
+		//CREATE NEW QUEUE AND ADD DEPENDENCIES
+		else if(add){
+				q = Queue(arr,{
+						id : id
+				});
+		}
+		//ERROR: CAN'T REMOVE FROM A QUEUE THAT DOES NOT EXIST
+		else{
+				return Config.debug("Cannot remove dependencies from a queue that does not exist.",this);
+		}
 
-    return q;
+		return q;
 },
 
 /**
--- test/define.js
+++ test/define.js
@@ -3,72 +3,78 @@
 //Code that runs in browser
 var fn = function(MochaTestRunnerDeferred){
 
-  console.info("Testing file: orgy.define...");
+	console.info("Testing file: orgy.define...");
 
-  var d1 = Orgy.define("somed-example",35);
+	var btrc = "define.js:8";
+	var d1 = Orgy.define("somed-example",35)._btrc(btrc);
+	tests.btrc(d1,btrc);
 
-  d1.then(function(r){
-    console.log("then1",r);
-    tests.then1(r,this.id);
-    return 36;
-  })
+	d1.then(function(r){
+		console.log("then1",r);
+		tests.then1(r,this.id);
+		return 36;
+	});
 
-  d1.then(function(r,def,last){
-    console.log("then2",r,last);
-    tests.then2(r,last);
-  },function(){
-    console.error("then2 rejected");
-    tests.then2();
-  })
+	d1.then(function(r,def,last){
+		console.log("then2",r,last);
+		tests.then2(r,last);
+	},function(){
+		console.error("then2 rejected");
+		tests.then2();
+	});
 
-  d1.done(function(r){
-    console.log("done",r);
-    tests.done(r)
-    //Makes the mocha test runner hold up execution.
-    if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
-  },function(){
-    console.error("done rejected");
-    tests.done()
-    //Makes the mocha test runner hold up execution.
-    if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
-  })
-}
+	d1.done(function(r){
+		console.log("done",r);
+		tests.done(r);
+		//Makes the mocha test runner hold up execution.
+		if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
+	},function(){
+		console.error("done rejected");
+		tests.done();
+		//Makes the mocha test runner hold up execution.
+		if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
+	});
+};
 
 //Test Runner Code
 if(typeof describe !== 'undefined'){
-  //Create a property with a true value after each chain segment,
-  //to keep track of execution order.
-  var output = {};
-  var tests = {
-    then1 : function(r){
-      r.should.equal(35);
-      should.not.exist(output.done);
-      should.not.exist(output.then2);
-      output.then1 = new Date().getTime();
-    },
-    then2 : function(r,last){
-      r.should.equal(35);
-      last.should.equal(36);
-      output.then2 = new Date().getTime();
-    },
-    done : function(r){
-      r.should.equal(35);
-      expect(output.then2).to.be.least(output.then1);
-      output.done = new Date().getTime();
-    }
-  };
-  describe('define.js tests',function(){
-    it('',function(done){
-      fn(done);
-    })
-  })
+	//Create a property with a true value after each chain segment,
+	//to keep track of execution order.
+	var output = {};
+	var tests = {
+		btrc : function(r,value){
+			expect(r).to.have.property('backtrace');
+			r.backtrace.should.equal(value);
+		},
+		then1 : function(r){
+			r.should.equal(35);
+			should.not.exist(output.done);
+			should.not.exist(output.then2);
+			output.then1 = new Date().getTime();
+		},
+		then2 : function(r,last){
+			r.should.equal(35);
+			last.should.equal(36);
+			output.then2 = new Date().getTime();
+		},
+		done : function(r){
+			r.should.equal(35);
+			expect(output.then2).to.be.least(output.then1);
+			output.done = new Date().getTime();
+		}
+	};
+	describe('define.js tests',function(){
+		it('',function(done){
+			fn(done);
+		})
+	})
 }
 else{
-  tests = {
-    then1 : function(){}
-    ,then2 : function(){}
-    ,done  : function(){}
-  }
-  fn();
+	tests = {
+		then1 : function(){}
+		,then2 : function(){}
+		,done  : function(){}
+	}
+	fn();
 }
 })()
