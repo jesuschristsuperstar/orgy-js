ccc02882f019037428cc0389dedf01572e93f8b9
tecfu <help@tecfu.com>
2015-07-08 03:33:27 -0400

    Updated unit tests.

--- test/queue.js
+++ test/queue.js
@@ -13,84 +13,81 @@ var fn = function(MochaTestRunnerDeferred){
 		{
 			type : "json"
 			,url : "data/data2.json"
-		},
-		{
-			type : "css"
-			,url : "data/sample.css"
-		},
-		{
-			type : "timer"
-			,timeout : 1000
 		}
-	],{
-		//timeout : 200  //optional. defaults to Orgy.config().timeout [5000]
-	});
+	]);
 
-	//Done can be called async and out of order.
-	setTimeout(function(){
-		q.done(function(r,deferred,last){
-			console.log("done",last);
-			tests.done(r,last);
-			//Makes the mocha test runner hold up execution.
-			if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
-		},function(){
-			console.error(this.id + " done rejected.");
-		});
-	},1000);
+	//Done can be called out of order.
+	q.done(function(r,deferred,last){
+		tests.done(r,deferred,last);
+		console.log(r,deferred,last);	
+		//Makes the mocha test runner hold up execution.
+		if(typeof MochaTestRunnerDeferred !== 'undefined') MochaTestRunnerDeferred();
+	},function(){
+		console.error(this.id + " done rejected.");
+	});
 
 
 	/**
 	 * If a then function returns a value, that value is passed down to any
 	 * subsequent then() or done() functions.
 	*/
-	q.then(function(r){
-			console.log("then1",r); //Dependency values.
-			tests.then1(r);
-			return -100.125;
+	q.then(function(r,deferred,last){
+		tests.then1(r,deferred,last);
+		return 1;
 	},function(){
 		console.error(this.id + " then 1 rejected.");
 	});
 
 	q.then(function(r,deferred,last){
-			console.log("then2",last); // 100.125
-			tests.then2(r,last);
-			return "abcdefg";
-	});
-}
+		tests.then2(r,deferred,last);
+		return last+1;
+	},function(){
+		console.error(this.id + " then 2 rejected.");
+	});}
 
 //Test Runner Code
 if(typeof describe !== 'undefined'){
-	//Create a property with a true value after each chain segment,
-	//to keep track of execution order.
-	var output = {};
+	//Track execution
+	var order = [];
 
 	var tests = {
-		then1 : function(r){
-			expect(r).to.have.length(4);
+		then1 : function(r,deferred,last){
+			//r
+			expect(r).to.have.length(2);
 			expect(r[0]).to.have.property('value');
 			r[0].value.should.equal(1);
 			expect(r[1]).to.have.property('value');
 			r[1].value.should.equal(2);
-			//@todo check css has been added to dom
-			should.not.exist(output.then2);
-			should.not.exist(output.done);
-			output.then1 = 1;
+	
+			//deferred
+			expect(deferred).to.have.property('resolve');	
+
+			//last
+			//For the first call to then, last is the dependency array
+			expect(last).to.equal(r);
+
+			//track/compare execution order
+			order.push('then1');
+
+			expect(order.length).to.equal(1);
 		},
-		then2 : function(r,last){
-			last.should.equal(-100.125);
-			should.not.exist(output.done);
-			output.then2 = 1;
+		then2 : function(r,deferred,last){
+			expect(r).to.have.length(2);
+			expect(deferred).to.have.property('resolve');	
+			last.should.equal(1);
+			
+			order.push('then2');
+			expect(order.length).to.equal(2);
+			expect(order.indexOf("then1")).to.equal(0);
 		},
-		done : function(r,last){
-			expect(r).to.have.length(4);
-			expect(r[0]).to.have.property('value');
-			r[0].value.should.equal(1);
-			expect(r[1]).to.have.property('value');
-			r[1].value.should.equal(2);
-			last.should.equal('abcdefg');
-			output.then1.should.equal(1);
-			output.then1.should.equal(1);
-			output.done = 1;
+		done : function(r,deferred,last){
+			expect(r).to.have.length(2);
+			expect(last).to.equal(2);	
+			
+			order.push('done');
+			expect(order.length).to.equal(3);
+			expect(order.indexOf("then1")).to.equal(0);
+			expect(order.indexOf("then2")).to.equal(1);
 		}
 	};
 	describe('queue.js tests',function(){
@@ -101,7 +98,8 @@ if(typeof describe !== 'undefined'){
 }
 else{
 	tests = {
-		then1 : function(){}
+		resolver : function(){}	
+		,then1 : function(){}
 		,then2 : function(){}
 		,done  : function(){}
 	}
