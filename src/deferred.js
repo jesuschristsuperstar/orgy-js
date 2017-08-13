cbae1331dd0c749107ebe8765531eec4469be4c5
tecfu <help@tecfu.com>
2017-08-13 02:47:44 -0700

    rewrite up and running

--- src/deferred.js
+++ src/deferred.js
@@ -7,7 +7,15 @@ module.exports.createId = createId;
 
 module.exports.deferred = function(resolve,reject,options){
 
+  options = options || {};
   const self = this;
+  let timeoutId = false; //handle so we can clear timeout on resolve
+
+  //set to usettled state
+  this.settled = false;
+
+  //track if timed out
+  this.timedOut = false;
 
   //create a unique id for this deferred
   this.id = createId();
@@ -16,20 +24,38 @@ module.exports.deferred = function(resolve,reject,options){
   this.state = 'pending'; //pending,resolved,rejected
   
   this.resolve = function(){
+    //do not re-run if already settled
+    if(self.settled){
+      throw new Error ('Cannot resolve a deferred that has already been settled.'); 
+    }
     self.state = 'resolved';
     resolve(arguments);
+    self.settled = true;
+    if(timeoutId){
+      clearTimeout(timeoutId);
+    }
   }
 
   this.reject = function(){
+    //do not re-run if already settled
+    if(self.settled){
+      throw new Error ('Cannot reject a deferred that has already been settled.'); 
+    }
     self.state = 'rejected'; 
     reject(arguments);
-  }
+    self.settled = true;
+    if(timeoutId){
+      clearTimeout(timeoutId);
+    }}
 
   if(options.timeout){
-    setTimeout(function(){
-      self.reject();
+    timeoutId = setTimeout(function(){
+      self.reject('timeout',options.timeout);
+      self.timedOut = true;
       //unset self.reject since already called
       self.reject = function(){};
     },options.timeout);
   }
+
+  return this;
 }
